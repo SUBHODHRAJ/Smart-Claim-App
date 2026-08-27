e7f54b85d2170ee8a8162385211d802f6d6c7c30
SUBHODHRAJ <subhodhraj1806@gmail.com>
2026-08-27 12:28:51 +0530

    chore: remove anti_ai_quiz from repo, keep only Ups/ (backend + frontend)

--- anti_ai_quiz/client/src/services/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './api';
-export * from './auth.service';
-export * from './document.service';
-export * from './quiz.service';
-export * from './ai.service';
-export * from './extra.service';
